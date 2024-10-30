01fe846604e349c6d6d15468709278caea2c2ccb
Guiheld <azevedodeheld@gmail.com>
2024-10-29 23:34:06 -0300

    ajustes do commit anterior

--- TrabalhoUemura/TrabalhoUemura/urls.py
+++ TrabalhoUemura/TrabalhoUemura/urls.py
@@ -23,7 +23,7 @@ from django.contrib import admin
 from django.urls import path, include
 from app import views
 
-from app.backupManager import importarDadosCSV
+from app.dadosManager import importarDadosCSV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
